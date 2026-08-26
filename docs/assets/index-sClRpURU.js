ede6039742694ac650b58361f3c50f017517ba89
junjun <373340904@qq.com>
2026-08-26 21:07:21 +0800

    加回首页/动态/任务/收藏功能，修复群主

--- docs/assets/index-sClRpURU.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-C4DT2vS5.js","./index-ChAhKQ2Y.js","./index-qLaKmnYz.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-ChAhKQ2Y.js";const _=r("Preferences",{web:()=>t(()=>import("./web-C4DT2vS5.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.PreferencesWeb)});export{_ as Preferences};
