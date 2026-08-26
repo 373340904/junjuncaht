d0917c4f11f4ac98db9df4ca953ce88b5a17a31c
junjun <373340904@qq.com>
2026-08-26 21:46:14 +0800

    修复管理员面板桌面端渲染

--- docs/dist-desktop/assets/index-zbhKKq5W.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CuAD05ff.js","./index-DYck-lES.js","./index-qLaKmnYz.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-DYck-lES.js";const _=r("Preferences",{web:()=>t(()=>import("./web-CuAD05ff.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.PreferencesWeb)});export{_ as Preferences};
